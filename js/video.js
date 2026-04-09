8645a30e414036c86bb68b0dfc660c2b03126383
Cara Anderson <carande@umich.edu>
2026-04-09 13:12:19 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,59 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+video=document.querySelector("#player1");
+video.autoplay=false;
+video.loop=false;
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+	document.querySelector("#volume").textContent=video.volume*100+"%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+    video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+    video.playbackRate = video.playbackRate * 0.9;
+    console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+    video.playbackRate = video.playbackRate * (1/0.9);
+    console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+    video.currentTime = video.currentTime + 10;
+    if (video.currentTime > video.duration) {
+        video.currentTime = 0;
+    }
+    console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    if (video.muted) {
+        video.muted = false;
+        document.querySelector("#mute").textContent = "Mute";
+    } else {
+        video.muted = true;
+        document.querySelector("#mute").textContent = "Unmute";
+    }
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+    video.volume = document.querySelector("#slider").value / 100;
+    document.querySelector("#volume").textContent = document.querySelector("#slider").value + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+    video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+});
